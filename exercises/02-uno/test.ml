ce0d3399efcd58c5b048bf8b221abea0048d54e8
Eric Mayhew <ejmayhew@gmail.com>
2022-04-20 17:38:55 -0400

    grader works hopefully

--- exercises/02-uno/test.ml
+++ exercises/02-uno/test.ml
@@ -1,1 +1,12 @@
 
+open Test_lib
+open Report 
+
+let () =
+  set_result @@
+  ast_sanity_check code_ast @@ fun () ->
+    [ Section
+      ( [ Text "Hi"; Code "this is code haha" ]
+      , [Message 
+      ( [ Text ("This is a text")], Success 1)] )
+    ]
