066250cd629e27fcad50e6b910cad74e573f0f91
Eric Mayhew <eric.mayhew@mail.mcgill.ca>
2022-05-03 16:33:13 -0400

    uno part 1: rework text, cut out functions
    Functions will go in the next lesson, where we can put the types from part 1
    into the prelude and use auto-grading for the functions.

--- exercises/02-uno/template.ml
+++ exercises/02-uno/template.ml
@@ -1,5 +1,5 @@
-(* TODO
-   Read description on the right and fill in the blanks! *)
+(* TODO: read description on the right and fill in the blanks! *)
+
 type color = Red | _ | _ | _ 
 
 type value = Numeric of int | Skip | _ | _ 
@@ -12,17 +12,3 @@ type player = hand
 type _ = _ 
 type _ = _ 
 
-(*=================================================*)
-(* Functions 
-   Please do not add type annontations to these functions for auto-grading reasons!*)
-let can_follow (color1, value1) (color2, value2) = 
-   _ || _ 
-
-let rec find_follow_ups hand card = 
-   raise NotImplemented
-
-let rec remove_card card hand = 
-   raise NotImplemented
-
-let rec add_card card pile = 
-   raise NotImplemented
