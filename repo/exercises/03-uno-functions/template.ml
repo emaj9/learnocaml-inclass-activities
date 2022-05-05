9f31cdd44b54b5016fb958568643a5a080031d4c
Eric Mayhew <eric.mayhew@mail.mcgill.ca>
2022-05-05 15:37:48 -0400

    wip uno part 2

--- repo/exercises/03-uno-functions/template.ml
+++ repo/exercises/03-uno-functions/template.ml
@@ -1,6 +1,9 @@
-(*=================================================*)
-(* Functions 
-   Please do not add type annontations to these functions for auto-grading reasons!*)
+(* Q1.
+ * Implement `can_follow` such that
+ *   can_follow c1 c2 = true
+ * only when c2 (understood to be coming from a player's hand)
+ * can be played on top of c1 (the top card of the discard pile).
+ *)
 let can_follow (color1, value1) (color2, value2) = 
    _ || _ 
 
