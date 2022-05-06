a9edc5434095c93315d135328843cae4db3fe2a4
Eric Mayhew <ejmayhew@gmail.com>
2022-05-06 19:49:03 -0400

    add parsons problems

---
+++ repo/exercises/03-parsons-problems/template.ml
@@ -0,0 +1,33 @@
+(*TODO 
+  Copy and paste the code lines in the right
+  into the functions here*)
+
+(* Q1 *)
+let curry f = 
+
+let uncurry f = 
+
+(* Q2 *)
+let is_prime n = 
+
+let rec all_primes a b =
+
+(* Q3 *)
+let mode l =
+	let rec aux l (cur_el, cur_num as cur_stats) (max_el, max_num as max_stats) =
+
+	in
+	match List.sort compare l with
+	
+
+(* Q4 *)
+(* Define Exception *)
+exception Change
+
+(* change: : int list -> int -> int list *)
+let rec change coins amt = 
+  (* Fill in *)
+  else 
+    begin match coins with 
+    (* Fill in *)
+    end
